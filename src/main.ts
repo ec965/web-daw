ced95302767125950ef848f3ff2258cb3e5f89dc
ec965 <22567131+ec965@users.noreply.github.com>
2022-05-09 22:47:42 -0700

    Trying out audio api

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,27 @@
 import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const audioCtx = new AudioContext();
+const osc = audioCtx.createOscillator();
+const gainNode = audioCtx.createGain();
+gainNode.gain.setValueAtTime(9, 1);
+
+osc.connect(gainNode).connect(audioCtx.destination);
+osc.start(9);
+
+const app = document.getElementById("app")!;
 
 app.innerHTML = `
-  <button>click me!</button>
+  <button id="play">play</button>
 `;
+
+const button = document.getElementById("play");
+
+if (button) {
+  button.onclick = () => {
+    if (audioCtx.state === "running") {
+      audioCtx.suspend();
+    } else {
+      audioCtx.resume();
+    }
+  };
+}
